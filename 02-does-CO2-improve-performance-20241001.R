4d90e1c3d29088d0642dbc91876b2d2b3ccfc01c
lpamminger <lucas.pamminger@monash.edu>
2024-12-09 17:15:59 +1100

    Making figure ready plots using patchwork

--- 02-does-CO2-improve-performance-20241001.R
+++ 02-does-CO2-improve-performance-20241001.R
@@ -4,7 +4,7 @@
 cat("\014") # clear console
 
 # Import libraries--------------------------------------------------------------
-pacman::p_load(tidyverse, sf)
+pacman::p_load(tidyverse, ozmaps, sf, patchwork)
 
 
 # Import functions -------------------------------------------------------------
@@ -42,26 +42,38 @@ gauge_information <- read_csv("./Data/Tidy/gauge_information_CAMELS.csv",
 
 
 
-aus_map <- read_sf(
-  dsn = "./Data/Maps",
-  layer = "AUS_2016_AUST"
-)
+# Making map -------------------------------------------------------------------
+aus_map <- ozmaps::ozmap(x = "states") |> 
+  filter(!NAME %in% c("Other Territories")) |> 
+  rename(state = NAME) |> 
+  mutate(
+    state = case_when(
+      state == "New South Wales" ~ "NSW",
+      state == "Victoria" ~ "VIC",
+      state == "Queensland" ~ "QLD",
+      state == "South Australia" ~ "SA",
+      state == "Western Australia" ~ "WA",
+      state == "Tasmania" ~ "TAS",
+      state == "Northern Territory" ~ "NT",
+      state == "Australian Capital Territory" ~ "ACT",
+    )
+  )
 
-vic_map <- read_sf(
-  dsn = "./Data/Maps",
-  layer = "vic_map_simple"
-)
+combine_NSW_ACT <- aus_map |> 
+  filter(state %in% c("NSW", "ACT")) |> 
+  st_union()
 
+aus_map[1,2] <- list(combine_NSW_ACT)
+  
+aus_map <- aus_map |> 
+  filter(state != "ACT")
 
-
-# Map plots --------------------------------------------------------------------
-# For a given catchment work out if a model with CO2 outperforms a model without CO2
-# remove unnecessary columns
-# only have unique streamflow model, gauge combinations
-# combine the streamflow model and objective function using unite
-# regex to see if the united columns contain "CO2"
-# if yes then contains_CO2 = TRUE otherwise contains_CO2 is false
-# for each catchment (gauge) get the two lowest AIC values by contains_CO2
+# Evidence ratio calculation ---------------------------------------------------
+# What to do (Taken from Burnham and Anderson 2002):
+## - assume non-CO2 is the best. best_CO2_AIC - best_non_CO2_AIC = AIC_diff. (This assumption does not impact the results. See chapter 2.11.2)
+## - Positive values mean best_non_CO2 is the better model. Negative values mean the best_CO2 model is better.
+## - calculate relative likelihood exp(0.5 * AIC_diff) pg. 72
+## - evidence ratio is a relative measure of how much better the model is over the other model
 
 best_CO2_and_non_CO2_per_catchment <- CMAES_results |>
   select(!c(parameter, parameter_value, optimiser, loglikelihood, exit_message, near_bounds)) |>
@@ -92,17 +104,6 @@ best_model_combination_per_catchment <- best_CO2_and_non_CO2_per_catchment |>
   )
 
 
-
-
-
-# Evidence ratio calculation ---------------------------------------------------
-# What to do (Taken from Burnham and Anderson 2002):
-## - assume non-CO2 is the best. best_CO2_AIC - best_non_CO2_AIC = AIC_diff. (This assumption does not impact the results. See chapter 2.11.2)
-## - Positive values mean best_non_CO2 is the better model. Negative values mean the best_CO2 model is better.
-## - calculate relative likelihood exp(0.5 * AIC_diff) pg. 72
-## - evidence ratio is a relative measure of how much better the model is over the other model
-
-
 evidence_ratio_calc <- best_CO2_and_non_CO2_per_catchment |>
   select(!c(streamflow_model_objective_function, streamflow_model, objective_function)) |>
   pivot_wider(
@@ -114,8 +115,8 @@ evidence_ratio_calc <- best_CO2_and_non_CO2_per_catchment |>
   ) |>
   mutate(
     evidence_ratio = case_when(
-      AIC_difference < 0 ~ -exp(0.5 * abs(AIC_difference)), # when CO2 model is better
-      AIC_difference > 0 ~ exp(0.5 * abs(AIC_difference)) # when non-CO2 model is better
+      AIC_difference < 0 ~ exp(0.5 * abs(AIC_difference)), # when CO2 model is better
+      AIC_difference > 0 ~ -exp(0.5 * abs(AIC_difference)) # when non-CO2 model is better
     )
   )
 
@@ -123,13 +124,18 @@ evidence_ratio_calc <- best_CO2_and_non_CO2_per_catchment |>
 
 
 
-# Plot time --------------------------------------------------------------------
+
+
+
+
+# Plot ready maps --------------------------------------------------------------
 plot_ready_data <- evidence_ratio_calc |>
   select(!c(CO2, no_CO2, AIC_difference)) |>
   left_join(
     gauge_information,
     by = join_by(gauge)
-  )
+  ) |> 
+  arrange(evidence_ratio)
 
 
 # I want the scale in these steps:
@@ -142,47 +148,56 @@ plot_ready_data <- evidence_ratio_calc |>
 # Extremely strong ->  >1,000,000
 
 
-
-## Victorian plot ==============================================================
-vic_evidence_ratio_map <- ggplot() +
+## By state ====================================================================
+evidence_ratio_by_state <- function(state, evidence_ratio_data, map_data) {
+  
+  # use state as key to extract correct polygon from map_data and evidence_ratio_data
+  state_map <- map_data |> 
+    filter(state == {{ state }})
+  
+  state_evidence_ratio <- evidence_ratio_data |> 
+    filter(state == {{ state }})
+  
+  ggplot() +
   geom_sf(
-    data = vic_map,
+    data = state_map,
     colour = "black",
     fill = "grey50"
   ) +
-  geom_point(
-    data = plot_ready_data |> filter(state == "VIC"),
-    aes(x = lon, y = lat, colour = evidence_ratio)
-  ) +
-  binned_scale( # binned scale code taken from: https://stackoverflow.com/questions/65947347/r-how-to-manually-set-binned-colour-scale-in-ggplot
-    aesthetics = "colour",
-    palette = function(x) c("#67001f", "#b2182b", "#d6604d", "#f4a582", "#fddbc7", "#f7f7f7", "#d1e5f0", "#92c5de", "#4393c3", "#2166ac", "#053061"), # length should be length(breaks + limits) - 1
-    breaks = c(-1E6, -1E4, -1E3, -1E2, -1E1, 1E1, 1E2, 1E3, 1E4, 1E6),
-    limits = c(-1E13, 1E13),
-    show.limits = FALSE,
-    guide = "coloursteps"
-  ) +
-  theme_bw() +
-  labs(
-    x = "Longitude",
-    y = "Latitude",
-    colour = "Evidence Ratio"
-  ) +
-  theme(
-    legend.key.height = unit(25, "mm")
-  )
+    geom_point(
+      data = state_evidence_ratio,
+      aes(x = lon, y = lat, colour = evidence_ratio),
+      show.legend = TRUE,
+      size = 0.5
+    ) +
+    binned_scale( # binned scale code taken from: https://stackoverflow.com/questions/65947347/r-how-to-manually-set-binned-colour-scale-in-ggplot
+      aesthetics = "colour",
+      palette = function(x) c("#f7f7f7", "#fddbc7", "#f4a582",  "#d6604d", "#b2182b", "#67001f"), # length should be length(breaks + limits) - 1
+      breaks = c(1E1, 1E2, 1E3, 1E4, 1E6),
+      limits = c(-1E1, 1E27),
+      show.limits = TRUE,
+      guide = "coloursteps"
+    ) +
+    theme_bw() +
+    labs(
+      x = "Longitude",
+      y = "Latitude",
+      colour = "Evidence Ratio"
+    ) +
+    theme(
+      legend.key.height = unit(25, "mm"),
+      legend.frame = element_rect(colour = "black")
+    )
+}
 
-ggsave(
-  filename = paste0("vic_evidence_ratio_map_", get_date(), ".pdf"),
-  plot = vic_evidence_ratio_map,
-  device = "pdf",
-  path = "./Graphs/CMAES_graphs",
-  height = 210,
-  width = 297,
-  units = "mm"
+by_state_plots <- map(
+  .x = aus_map |> pull(state) |> unique(),
+  .f = evidence_ratio_by_state,
+  evidence_ratio_data = plot_ready_data,
+  map_data = aus_map
 )
 
-# Australian plot ==============================================================
+## By country ==================================================================
 aus_evidence_ratio_map <- ggplot() +
   geom_sf(
     data = aus_map,
@@ -193,14 +208,14 @@ aus_evidence_ratio_map <- ggplot() +
   geom_point(
     data = plot_ready_data,
     aes(x = lon, y = lat, colour = evidence_ratio),
-    size = 1
+    size = 0.5
   ) +
   binned_scale( # binned scale code taken from: https://stackoverflow.com/questions/65947347/r-how-to-manually-set-binned-colour-scale-in-ggplot
     aesthetics = "colour",
-    palette = function(x) c("#67001f", "#b2182b", "#d6604d", "#f4a582", "#fddbc7", "#f7f7f7", "#d1e5f0", "#92c5de", "#4393c3", "#2166ac", "#053061"), # length should be length(breaks + limits) - 1
-    breaks = c(-1E6, -1E4, -1E3, -1E2, -1E1, 1E1, 1E2, 1E3, 1E4, 1E6),
-    limits = c(-1E13, 1E13),
-    show.limits = FALSE,
+    palette = function(x) c("#f7f7f7", "#fddbc7", "#f4a582",  "#d6604d", "#b2182b", "#67001f"), # length should be length(breaks + limits) - 1
+    breaks = c(1E1, 1E2, 1E3, 1E4, 1E6),
+    limits = c(-1E1, 1E27),
+    show.limits = TRUE,
     guide = "coloursteps"
   ) +
   theme_bw() +
@@ -210,18 +225,36 @@ aus_evidence_ratio_map <- ggplot() +
     colour = "Evidence Ratio"
   ) +
   theme(
-    legend.key.height = unit(25, "mm")
+    legend.key.height = unit(25, "mm"),
+    legend.frame = element_rect(colour = "black")
   )
 
+
+
+
+## Combine to make a paper ready plot ==========================================
+# make it look nice
+nice_plot <- reduce(.x = by_state_plots, .f = `+`) + aus_evidence_ratio_map + plot_layout(guides = 'collect')
+nice_plot
 ggsave(
-  filename = paste0("aus_evidence_ratio_map_", get_date(), ".pdf"),
-  plot = aus_evidence_ratio_map,
+  filename = paste0("nice_plot_", get_date(), ".pdf"),
+  plot = nice_plot,
   device = "pdf",
   path = "./Graphs/CMAES_graphs",
   height = 210,
   width = 297,
   units = "mm"
 )
+
+
+
+
+
+
+
+
+
+
 
 
 
