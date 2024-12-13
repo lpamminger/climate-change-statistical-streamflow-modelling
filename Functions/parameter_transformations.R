67509dd25a316bf59d8cb8ca19a72375312dddff
lpamminger <lucas.pamminger@monash.edu>
2024-12-13 15:43:43 +1100

    Adding functions to convert real space to transformed space

--- Functions/parameter_transformations.R
+++ Functions/parameter_transformations.R
@@ -17,7 +17,7 @@ no_parameter_transform <- function(parameter_set, lower_bound = NULL, upper_boun
   parameter_set
 }
 
-# Transform parameters from 0-10 to real parameter values
+# Transform parameters from 0-scale to real parameter values
 transform_parameter_method <- function(method, idx, lower_bound, upper_bound, parameter_set, scale) {
   method <- noquote(method)
   
@@ -25,4 +25,17 @@ transform_parameter_method <- function(method, idx, lower_bound, upper_bound, pa
   # cols = different combination of parameters and rows = parameter
   # only apply transform methods to rows (the same parameters)
   method(parameter_set = parameter_set[idx,], lower_bound = lower_bound, upper_bound = upper_bound, scale = scale)
+}
+
+
+
+
+
+# Converting real space into transformed space ---------------------------------
+real_space_to_scaled_linear_transform <- function(real_space_parameter, lower_bound, upper_bound, scale) {
+  (scale * (real_space_parameter - lower_bound)) / (upper_bound - lower_bound)
+}
+
+real_space_to_scaled_logarithmic_transform <- function(real_space_parameter, lower_bound, upper_bound, scale) {
+  (scale * log(real_space_parameter / lower_bound)) / (log(upper_bound / lower_bound))
 }
