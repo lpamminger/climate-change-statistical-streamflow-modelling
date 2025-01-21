03765a0e14d31155b71b2ba911eb4e8bdb909b22
Lucas Pamminger <lucas.pamminger@monash.edu>
2025-01-21 13:07:29 +1100

    Futher testing of parameters. The CO2 models seem to be the source of problems. I think redoing CMAES with limited a5 parameter is worth doing.

--- 03-dream-model-fitting.R
+++ 03-dream-model-fitting.R
@@ -2,7 +2,7 @@
 cat("\014") # clear console
 
 # Import libraries--------------------------------------------------------------
-pacman::p_load(tidyverse, dream, coda, lattice, tictoc, furrr, parallel, truncnorm, sloop)
+pacman::p_load(tidyverse, dream, coda, lattice, tictoc, furrr, parallel, truncnorm, sloop, snow)
 # install dream using: install.packages("dream", repos="http://R-Forge.R-project.org")
 # install.packages("coda")
 
@@ -122,15 +122,18 @@ gauge_streamflow_model_objective_function_combinations <- best_model_combination
 
 # Produce numerical_optimiser_objects ready for the optimiser ------------------
 numerical_optimiser_objects <- function(gauge, streamflow_model, objective_function, data, start_stop) {
-  gauge |>
+  
+  catchment_data_set <- gauge |>
     catchment_data_blueprint(
       observed_data = data,
       start_stop_indexes = start_stop
-    ) |>
+    ) 
+  
+  catchment_data_set |> 
     numerical_optimiser_setup_vary_inputs(
       streamflow_model = streamflow_model,
       objective_function = objective_function,
-      bounds_and_transform_method = make_default_bounds_and_transform_methods(),
+      bounds_and_transform_method = make_default_bounds_and_transform_methods(catchment_data_set), # this must adjust for CO2 from catchment_data_set
       minimise_likelihood = FALSE
     )
 }
@@ -146,6 +149,9 @@ ready_for_optimisation <- pmap(
 
 
 
+# See if the change in make_default_bounds_and_transform_methods works
+
+
 
 
 
@@ -157,6 +163,7 @@ chunked_ready_for_optimisation <- split( # required for chunking in parallel
   ready_for_optimisation,
   ceiling(seq_along(ready_for_optimisation) / CHUNK_SIZE)
 )
+
 
 
 
@@ -184,10 +191,13 @@ chunked_ready_for_optimisation <- split( # required for chunking in parallel
 
 
 # Learning from trace plots:
-## 1. Single catchment did not converge (121003A)
+
+## First (mk_1)
+## 1. Single catchment did not converge (121003A). Failed in burn-in (burn.in flag is TRUE)
 ## 2. I think there are insufficient chains for some catchments. Not enough wiggle
 ##    Up default chain number from (2 * parameter_number) + 1 to
 ##    3 * parameter_number
+
 
 ## Second (mk_2)
 ## 1. Still not enough wiggle for some catchments. For example, 107001B is 
@@ -200,9 +210,9 @@ chunked_ready_for_optimisation <- split( # required for chunking in parallel
 ## Third (mk_3)
 ## 1. Looked at the DREAM (vrugt) and DREAM (r code).
 ## 2. steps represents the number of generations. In DREAM manual its > 1000
-##    increase to 200 for testing.
+##    increase to 100 for testing.
 ## 3. nCR alters pCR  (probability of cross-over). As nCR increases pCR decreases.
-##    Small pCR means smaller steps.
+##    Small pCR means smaller steps. Stick to 3.
 ## 4. gamma does not do anything when using logposterior.density. So we ignore it.
 ## 5. There are two eps values in "offde" function. This reminds me of 
 ##    kernel_adapt from fmcmc. fmcmc documentation: eps both sets the initial 
@@ -211,12 +221,53 @@ chunked_ready_for_optimisation <- split( # required for chunking in parallel
 ##    and ensures the variance-covariance is greater than zero. 
 ##    The fixed eps (not user input) looks like it ensures variance-cov 
 ##    is greater than zero. From testing a larger eps equals larger amplitude of
-##    wiggles.
+##    wiggles. Change eps from 0.05 to 0.1
 
-# Is sem number of generations (T in matlab)?
+## Lessons from mk 3
+## 1. increasing steps improves mixing.
+## 2. Bad catchments are: 
+##    - 121003A (did not converge, poor acceptance rate, poor mixing, not stationary)
+##    - 122004A (poor mixing - improved either with steps, chains or nCR)
+## 121003A performs poorly work out why.
+## 3. increasing steps does not increase iterations
 
-## 2. Gauge 121003A still has not converged. Ran out of iterations. 
-##    Failed in burn-in (burn.in flag is TRUE)
+## Maybe try running for all catchments - identify bad catchments, then see
+## what works.
+## a3 is not good if a5 > max_CO2. Maybe limit a5 to max CO2 to 
+## force a3 to be meaningful? Get close enough to max. Since each 
+## gauge ends at a different time it will vary.
+## Based off distribution results and trace diagrams a second pass into 
+## dream may not be required. If the traces are good, leave it.
+
+## Forth (mk_4)
+## 1. I want to get 121003A to converge. Traces are still not stationary.
+## 2. Method to try and get 121003A to converge:
+##    - increase eps, increase nCR, adjust bounds, increase iterations, check if model is correct
+## 3. I am not concerned with 122004A. Cranking steps should fix the issue.
+
+## Default (check_convergence_steps = 1000, warm_up_per_chain = 5E4, burn_in_per_chain = 5E3, iterations_after_burn_in_per_chain = 5E3, nCR = 3, eps = 0.1, steps = 10,
+## Test 1: increase nCR from 3 to 6 --> result: no noticeable impact
+## Test 2: increase eps from 0.1 to 0.5 --> result: worse for a0_d and a5
+## Test 3: decrease eps from 0.1 to 0.02 --> result: no major improvement
+## Test 4: lower a5 bound to max CO2 in t-series (136.41) --> result: extreme improvement. With steps can get hairy caterpillar 
+## Test 5: can I get away with lowering a5 bound to naive max (last CO2 not for the t-series 138.53) --> result: just as good. But likely to break if t-series ends well before 2022
+## Test 6: full run with a5 = 136.41 (check_convergence_steps = 1000, warm_up_per_chain = 5E4, burn_in_per_chain = 1E4, iterations_after_burn_in_per_chain = 1E4, nCR = 3, eps = 0.1, steps = 100)
+##         test 6 results -> not great more wiggle required, low acceptance rate and did not converge
+## Test 7: increase nCR to 8 (param #), steps = 10, way better.
+## Test 8: increase nCR to 16 - no meaningful change
+## Test 9: nCR = 8, limit bounds, eps = c(0.02, 0.1, 0.5) does it matter? Yes, 0.5 too much and 0.02 is too little. I think stick to 0.1. Both 0.02 and 0.1 converged.
+## Test 10: Vary chains. 4 * param number. Does it matter? Yes, more chains = better. Did not converge.
+## Test 11: Full gas. nCR = param, limit bounds, eps = 0.1, steps = 500, double iterations for everything
+##          results - Everything is good except for the CO2 models. I think
+##          I should redo the CMAES with the adjusted limits or at least test them
+
+
+# idea: make nCR equal to hte number of parameters?
+
+## Lessons from mk_4
+## 1. a5 bound must be adjusted to the maximum CO2 for the given catchment.
+##    This means the a5 upper bound must change for each catchment - recoding required
+
 
 # From dream manual
 # nCR = 3 works well in practice but larger nCR maybe required for high-dimensional problems (d > 50)
@@ -226,8 +277,9 @@ chunked_ready_for_optimisation <- split( # required for chunking in parallel
 # gamma is used in CalcCbWb(gamma) equation Thiemann et al. WRR 2001 equation 20. I don't think it is connected to anything? See GenCR function
 # steps seems like T value from matlab (number of generations)
 # maybe a side-by-side of code is required to work out what parameters do what.
-# steps is the number of times through loops. This looks the same as DREAM. How did vrugt pick T in manual?
+# steps is the number of times through loops. This looks the same as DREAM. I think it represents number of samples from the prior distribution. How did vrugt pick T in manual?
 # gamma = kurtosis parameter baysian (is this the shaping factor in manual)
+# "rand" bounds handling is good for me - fold is preferred statistically (dream manual) but if posterior is on edege of search domain problems
 
 # functions that take control as an input
 # GenCR (only nCR), AdaptCR (), metrop, CompDensity (gamma, Wb, Cb), offde, 
@@ -265,29 +317,29 @@ chunked_ready_for_optimisation <- split( # required for chunking in parallel
 # For non-converged either increase iterations or 
 
 ## An efficient way to see if the changes have worked is test the single catchments
-#test_catchment <- chunked_ready_for_optimisation[[1]][[11]] # 11 = non-converging (700 sec steps = 200, max iter limit)
+#test_catchment <- chunked_ready_for_optimisation[[1]][[8]] # 11 = non-converging (700 sec steps = 200, max iter limit)
 
-# does increasing steps increase iterations? Try using [[1]] 
-# increasing steps does not increase iterations
 
-#tic()
+
+
+
 #set.seed(1)
 #test_converged_dream <- DREAM(
-#  input = test_catchment,
+ # input = test_catchment,
 #  controls = list(
-#    check_convergence_steps = 1000,
-#    #warm_up_per_chain = 1E5,
-#    #burn_in_per_chain = 2E4, 
-#    #iterations_after_burn_in_per_chain = 2E4, 
-#    nCR = 3,
-#    eps = 0.1,
-#    steps = 7,
-#    bound_handling = "rand" # rand is good for me - fold is preferred statistically (dream manual) but if posterior is on edege of search domain problems
-#    )
+ #   check_convergence_steps = 1000,
+  #  warm_up_per_chain = 1E5,
+  #  burn_in_per_chain = 1E4, 
+  #  iterations_after_burn_in_per_chain = 1E4, 
+  #  nCR = 8,
+  #  eps = 0.1,
+  #  steps = 10 
+  #  )
 #)
-#toc()
+
 
 # nCR = 3, eps = 0.1 and steps = 7 = convergences for non-converging catchment, chain = 3 * param number 
+#test_converged_dream$time
 #gg_trace_plot(test_converged_dream)
 #get_convergence_statistics(test_converged_dream)
 
@@ -301,17 +353,17 @@ CHUNK_ITER <- 1 # length(chunked_ready_for_optimisation)
 
 plan(multisession, workers = length(availableWorkers())) # set once for furrr
 
+tic()
 converged_dream_objects <- future_map(
   .x = chunked_ready_for_optimisation[[CHUNK_ITER]], # this must be repeated for all chunks
   .f = DREAM,
   controls = list(
     check_convergence_steps = 1000,
     warm_up_per_chain = 1E5,
-    burn_in_per_chain = 2E4, 
-    iterations_after_burn_in_per_chain = 2E4, 
-    nCR = 3,
+    burn_in_per_chain = 3E4, 
+    iterations_after_burn_in_per_chain = 3E4, 
     eps = 0.1,
-    steps = 200,
+    steps = 500
     ),
   .options = furrr_options(
     seed = TRUE
@@ -320,14 +372,14 @@ converged_dream_objects <- future_map(
     )
   )
 
-
+toc()
 converged_trace_plots <- map( 
   .x = converged_dream_objects,
   .f = gg_trace_plot
 ) 
 
 ggsave(
-  filename = paste0("trace_plots_chunk_", CHUNK_ITER, "_", get_date(), ".pdf"),
+  filename = paste0("test_11_trace_plots_chunk_", CHUNK_ITER, "_", get_date(), ".pdf"),
   plot = gridExtra::marrangeGrob(converged_trace_plots, nrow = 1, ncol = 1),
   device = "pdf",
   path = "./Graphs/DREAM_graphs",
@@ -343,12 +395,16 @@ converged_stats <- map(
 ) |> 
   list_rbind() |> 
   write_csv(
-    file = paste0("./Results/my_dream/converged_stats_chunk_", CHUNK_ITER, "_", get_date(), ".csv")
+    file = paste0("./Results/my_dream/test_11_converged_stats_chunk_", CHUNK_ITER, "_", get_date(), ".csv")
   )
 
 
 stop_here <- 1
 
+gg_distribution_plot(converged_dream_objects[[8]])
+
+# based on the look of the distributions and trace diagrams, I might
+# not every need the second pass.
 
 # put back into DREAM and run without convergences to build the distribution?
 test_distribution_DREAM <- tactical_typo_DREAM(
