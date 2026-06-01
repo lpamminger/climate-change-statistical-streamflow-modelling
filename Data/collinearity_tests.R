d8fc79bdbfbdfad89d0bc35fe2b5a7fff71c4b28
lpamminger <lucas.pamminger@monash.edu>
2026-06-01 13:17:56 +1000

    Adding temperature to collinearity tests. Plotting VIF boxplots for extended figures.

--- Data/collinearity_tests.R
+++ Data/collinearity_tests.R
@@ -1,7 +1,7 @@
 # Examine the collinearity between rainfall and CO2
 
 # Import libraries required ----------------------------------------------------
-pacman::p_load(tidyverse, olsrr)
+pacman::p_load(tidyverse, olsrr, patchwork)
 
 
 # Import functions -------------------------------------------------------------
@@ -51,7 +51,7 @@ mortons_wet_annual <- mortons_wet_daily |>
   ) |>
   summarise(
     APET = sum(APET),
-    #n = n(),
+    n = n(),
     .by = c(year, gauge)
   ) |>
   # filter out incomplete years
@@ -64,6 +64,66 @@ data <- data |>
     mortons_wet_annual,
     by = join_by(gauge, year)
   )
+
+
+# add temperature (tmax) to the data -------------------------------------------
+# use maximum daily temperature - AGCD temperature (there is also SILO) I am not sure of the difference
+daily_tmax <- read_csv(
+  "Data/Raw/tmax_AGCD.csv",
+  show_col_types = FALSE
+)
+
+
+annual_tmax <- daily_tmax |>
+  pivot_longer(
+    cols = !c(year, month, day),
+    names_to = "gauge",
+    values_to = "tmax"
+  ) |>
+  summarise(
+    tmax = mean(tmax),
+    n = n(),
+    .by = c(year, gauge)
+  ) 
+  # there are no incomplete years
+
+
+
+
+
+
+## join to data
+## Annual tmax is the maximum observed temperature in a given year per gauge
+data <- data |> 
+  left_join(
+    annual_tmax,
+    by = join_by(gauge, year)
+  ) |> 
+  select(!c(n.x, n.y))
+
+
+CO2_vs_temperature_plot <- data |> 
+  ggplot(aes(x = CO2, y = tmax)) +
+  geom_point() +
+  geom_smooth(
+    formula = y ~ x,
+    method = lm,
+    colour = "red",
+    se = FALSE
+  ) +
+  theme_bw() +
+  facet_wrap(~gauge, scales = "free_y") 
+
+
+ggsave(
+  filename = "./Figures/Other/CO2_mean_max_daily_temp_correlation_plot.pdf",
+  plot = CO2_vs_temperature_plot,
+  device = "pdf",
+  width = 1189,
+  height = 841,
+  units = "mm"
+)
+
 
 # Get gauges with moderately_strong (100) or greater ---------------------------
 evidence_ratio <- best_CO2_non_CO2_per_gauge |>
@@ -121,6 +181,7 @@ correlation_of_p_mm_and_CO2 <- data |>
     R2_round = paste0("R^2==", signif(R2, digits = 2))
   ) |> 
   arrange(desc(R2))
+
 
 
 ## Summary statistic of correlation and R2 =====================================
@@ -184,6 +245,150 @@ ggsave(
 
 
 
+# Correlation between all independent variables --------------------------------
+# Correlation matrix per catchment 
+
+make_correlation_matrix_per_gauge <- function(gauge, data) {
+  
+  # if there is not drought it returns a warning
+  
+  data |> 
+    filter(gauge == {{ gauge }}) |>
+    # avoid NA's being an issue
+    filter(included_in_calibration) |> 
+    # only select independent variables
+    select(
+      p_mm, 
+      standardised_warm_season_to_annual_rainfall_ratio,
+      CO2,
+      drought,
+      APET,
+      tmax
+    ) |>
+    as.matrix() |> 
+    cor()
+  
+}
+
+
+correlation_matrix_per_gauge <- map(
+  .x = data |> pull(gauge) |> unique(),
+  .f = make_correlation_matrix_per_gauge,
+  data = data
+) |> 
+  # convert to 3D array
+  simplify2array()
+# warnings occur because there are catchment without drought be corr
+
+
+## Find the mean, median and standard deviation for across all matrices --------
+count_greater_than_abs <- function(x, value) {
+  boolean_vector <- abs(x) > value
+  return(sum(boolean_vector, na.rm = TRUE))
+}
+
+apply(
+  correlation_matrix_per_gauge,
+  MARGIN = c(1, 2),
+  FUN = count_greater_than_abs, # change values to test
+  simplify = TRUE,
+  # correlations are subject - I selected 0.6
+  value = 0.6
+  # required for the drought/non-drought catchments
+  #na.rm = TRUE
+  ) |> 
+  round(digits = 3)
+
+# address problematic values 
+# - there are 3 catchments with a CO2 and APET corr > |0.6|
+# - 15 catchment with CO2 and drought corr > |0.6|
+# - 5 catchments with drought + APET > 0.6
+ 
+
+# do they have a high evidence ratio?
+cor_CO2_APET <- data |> 
+  filter(included_in_calibration) |> 
+  summarise(
+    cor = cor(CO2, APET),
+    .by = gauge
+  ) |> 
+  filter(abs(cor) > 0.6)
+
+gauges_with_drought <- gauge_information |> 
+  filter(drought) |> 
+  pull(gauge)
+
+
+cor_drought_CO2 <- data |> 
+  filter(included_in_calibration) |> 
+  filter(gauge %in% gauges_with_drought) |> 
+  mutate(
+    drought = as.numeric(drought)
+  ) |> 
+  summarise(
+    cor = cor(CO2, drought),
+    .by = gauge
+  ) |> 
+  filter(abs(cor) > 0.6)
+
+
+cor_drought_APET <- data |> 
+  filter(included_in_calibration) |> 
+  filter(gauge %in% gauges_with_drought) |> 
+  summarise(
+    cor = cor(drought, APET),
+    .by = gauge
+  ) |> 
+  filter(abs(cor) > 0.6)
+
+
+
+
+# - identify catchments cor_CO2_APET |> pull(gauge)
+high_corr_gauges <- cor_CO2_APET |> #rbind(cor_CO2_APET, cor_drought_CO2, cor_drought_APET) |> 
+  pull(gauge) |> 
+  unique()
+
+# - do they have a high evidence ratio? 
+evi_ratio_high_corr_gauges <- data |> 
+  filter(gauge %in% high_corr_gauges) |> 
+  select(gauge, evidence_ratio) |> 
+  distinct() |> 
+  arrange(desc(evidence_ratio))
+
+# - if so investigate streamflow time 
+# I am not convinced with the drought stuff
+data |> 
+  filter(gauge %in% high_corr_gauges) |> 
+  ggplot(aes(y = CO2, x = as.numeric(drought))) +
+  geom_point() +
+  geom_smooth(formula = y ~ x, method = lm) +
+  theme_bw() +
+  facet_wrap(~gauge, scales = "free_y")
+
+
+
+
+median_correlation_matrix <- apply(
+  correlation_matrix_per_gauge,
+  MARGIN = c(1, 2),
+  FUN = median,
+  simplify = TRUE,
+  na.rm = TRUE
+) |> 
+  round(digits = 3)
+
+
+sd_correlation_matrix <- apply(
+  correlation_matrix_per_gauge,
+  MARGIN = c(1, 2),
+  FUN = sd,
+  simplify = TRUE,
+  na.rm = TRUE
+) |> 
+  round(digits = 3)
+
+
 
 # Calculate the Variance Inflation Factor --------------------------------------
 
@@ -199,7 +404,7 @@ ggsave(
 # return tibble - append gauge to leftmost column
 
 
-VIF_assessment <- function(gauge, data) {
+VIF_assessment <- function(gauge, independent_variables, data) {
   
   # filter by gauge
   gauge_data <- data |> 
@@ -209,13 +414,20 @@ VIF_assessment <- function(gauge, data) {
   # check if any drought is TRUE
   drought_check <- gauge_data |> pull(drought) |> any()
   
+  
   if(drought_check) {
     
     # need to convert drought to factor for lm
     drought_gauge_data <- gauge_data |> 
       mutate(drought = as.factor(drought))
+    
+    # add drought to independent variables
+    independent_variables <- c("drought", independent_variables)
+    
+    # convert character vector into formula
+    formula <- reformulate(termlabels = independent_variables, response = "q_mm")
       
-    lm(q_mm ~ drought + p_mm + standardised_warm_season_to_annual_rainfall_ratio + CO2 + APET, data = drought_gauge_data) |> 
+    lm(formula = formula, data = drought_gauge_data) |> 
       ols_vif_tol() |> 
       add_column(
         gauge = {{ gauge }},
@@ -224,7 +436,10 @@ VIF_assessment <- function(gauge, data) {
     
   } else {
     
-    lm(q_mm ~ p_mm + standardised_warm_season_to_annual_rainfall_ratio + CO2 + APET, data = gauge_data) |> 
+    # convert character vector into formula
+    formula <- reformulate(termlabels = independent_variables, response = "q_mm")
+    
+    lm(formula = formula, data = gauge_data) |> 
       ols_vif_tol() |> 
       add_column(
         gauge = {{ gauge }},
@@ -235,45 +450,130 @@ VIF_assessment <- function(gauge, data) {
 }
 
 
+## Repeat VIF assessment for different combinations of variables ===============
 
-VIF_results <- map(
+### VIF of independent variables used in analysis ##############################
+VIF_results_1 <- map(
   .x = data |> pull(gauge) |> unique(),
   .f = VIF_assessment,
+  independent_variables = c("p_mm", "standardised_warm_season_to_annual_rainfall_ratio", "CO2"), #exclude drought
   data = data
 ) |> 
-  list_rbind() 
+  list_rbind() |> 
+  add_column(test = "default_variables")
 
-# Look at VIF results
-VIF_results |> 
+# only 1 gauge over 4
+VIF_results_1 |> 
   filter(VIF > 4)
 
-# problem gauge = 701007 (near zero annual flow)
-# 208026, 121003A
-#data |> 
-#  filter(gauge %in% c("701007", "208026", "121003A")) |> 
-#  ggplot(aes(x = year, y = q_mm)) + 
-#  geom_line() +
-#  facet_wrap(~gauge, scales = "free_y", nrow = 3)
+
+
+
+### VIF of independent variables used in analysis and APET #####################
+VIF_results_2 <- map(
+  .x = data |> pull(gauge) |> unique(),
+  .f = VIF_assessment,
+  independent_variables = c("p_mm", "standardised_warm_season_to_annual_rainfall_ratio", "CO2", "APET"), #exclude drought
+  data = data
+) |> 
+  list_rbind() |> 
+  add_column(test = "with_APET")
+
+# only 2 gauge over 4
+VIF_results_2 |> 
+  filter(VIF > 4)
+
+
+
+
+### VIF of independent variables used in analysis and temp #####################
+VIF_results_3 <- map(
+  .x = data |> pull(gauge) |> unique(),
+  .f = VIF_assessment,
+  independent_variables = c("p_mm", "standardised_warm_season_to_annual_rainfall_ratio", "CO2", "tmax"), #exclude drought
+  data = data
+) |> 
+  list_rbind() |> 
+  add_column(test = "with_tmax")
+
+# bigggg issues
+VIF_results_3 |> 
+  filter(VIF > 4)
+
+
+
+
+
+### VIF of independent variables used in analysis, APET and temp ###############
+VIF_results_4 <- map(
+  .x = data |> pull(gauge) |> unique(),
+  .f = VIF_assessment,
+  independent_variables = c("p_mm", "standardised_warm_season_to_annual_rainfall_ratio", "CO2", "tmax", "APET"), #exclude drought
+  data = data
+) |> 
+  list_rbind() |> 
+  add_column(test = "with_APET_and_tmax")
+
+# only 1 gauge over 4
+VIF_results_4 |> 
+  filter(VIF > 4)
+
+
+
+
+# make a 4 x 4 VIF
+VIF_results <- rbind(VIF_results_1, VIF_results_2, VIF_results_3, VIF_results_4)
+
+# add abc to facet
+# strip facet labels
+
+# tibble with x, y, test and label_name
+# will be the same between plots, y will be max(VIF)
+abc_labels <- VIF_results |> 
+  summarise(
+    y_pos = max(VIF),
+    .by = test
+  ) |> 
+  mutate(x_pos = "APET") |> 
+  mutate(label_name = c("a", "b", "c", "d"))
+
+
 
 VIF_plot <- VIF_results |> 
   ggplot(aes(x = Variables, y = VIF)) +
   geom_boxplot(
     staplewidth = 0.5
   ) +
+  geom_hline(yintercept = 4, colour = "red", linetype = "dashed") +
+  geom_text(
+    aes(x = x_pos, y = y_pos, label = label_name),
+    data = abc_labels,
+    fontface = "bold",
+    hjust = 2
+    )+
   labs(
     y = "Variance Inflation Factor (VIF)",
     x = "Independent Variables"
   ) +
   scale_x_discrete(
-    labels = c("APET", bquote(CO[2]), "Drought", "Precipitation", "Rainfall Seasonality")
+    labels = c("APET", bquote(CO[2]), "Drought", "Precipitation", "Rainfall Seasonality", "Temperature")
   ) +
-  theme_bw()
+  theme_bw() +
+  theme(
+    strip.background = element_blank(),
+    strip.text = element_blank(),
+    axis.text = element_text(size = 8)
+  ) +
+  facet_wrap(~test, scales = "free_y")
+
+
+
 
 ggsave(
   filename = "./Figures/Other/VIF_independent_variables_boxplot.pdf",
   plot = VIF_plot,
   device = "pdf",
-  width = 250,
+  width = 280,
   height = 200,
   units = "mm"
 )
