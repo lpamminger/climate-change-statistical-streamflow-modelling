9dde134b5af54ffdff17d244b0dd042559b14d1a
lpamminger <lucas.pamminger@monash.edu>
2026-07-01 11:42:51 +1000

    Make NSE ecdf for supplementary

--- Modelling/explore_streamflow_changes.R
+++ Modelling/explore_streamflow_changes.R
@@ -1,5 +1,5 @@
-# Purpose of script: 
-# 1. create streamflow timeseries for counterfactual 
+# Purpose of script:
+# 1. create streamflow timeseries for counterfactual
 # 2. Plot streamflow time and rainfall-runoff plots of all gauges
 
 
@@ -10,8 +10,6 @@
 
 # Import libraries--------------------------------------------------------------
 pacman::p_load(tidyverse, truncnorm, sloop)
-
-
 
 
 # Import functions -------------------------------------------------------------
@@ -27,8 +25,6 @@ source("./Functions/DREAM.R")
 source("./Functions/objective_function_setup.R")
 source("./Functions/result_set.R")
 source("./Functions/boxcox_logsinh_transforms.R")
-
-
 
 
 # Import data ------------------------------------------------------------------
@@ -69,16 +65,12 @@ streamflow_results <- read_csv(
 )
 
 
-
-
-
-
 # Calculate counterfactual for all models with CO2 in them ---------------------
 
 ## Set a3 terms (slope and intercept to zero) ==================================
 only_CO2_model_parameters <- best_CO2_non_CO2_per_gauge |>
   filter(str_detect(streamflow_model, "CO2"))
-  
+
 
 set_a3_zero_model_parameters <- only_CO2_model_parameters |>
   mutate(
@@ -115,7 +107,6 @@ streamflow_name_and_model_for_joining <- map(
   .f = get_model_name_and_function
 ) |>
   list_rbind()
-
 
 
 zero_CO2_streamflow_models <- set_a3_zero_model_parameters |>
@@ -158,11 +149,6 @@ altered_streamflow <- pmap(
   .f = generate_altered_streamflow
 ) |>
   list_rbind()
-
-
-
-
-
 
 
 ###  Summary table #############################################################
@@ -211,30 +197,28 @@ streamflow_data_a3_off <- altered_streamflow |>
   )
 
 
-
-
 ## Convert a3_off_transformed_modelled streamflow into realspace ===============
 ### Pull a part streamflow_data_a3_off and put it back together
 
 convert_a3_off_transformed_to_realspace <- function(gauge, streamflow_data_a3_off, best_model_per_gauge) {
   filtered_streamflow_data_a3_off <- streamflow_data_a3_off |>
     filter(gauge == {{ gauge }})
-  
+
   a3_off_transformed_streamflow <- filtered_streamflow_data_a3_off |>
     pull(a3_off_modelled_transformed_streamflow)
-  
+
   parameters <- best_model_per_gauge |>
     filter(gauge == {{ gauge }}) |>
     pull(parameter_value)
-  
+
   b <- parameters[length(parameters) - 1]
-  
+
   a3_off_realspace_streamflow <- inverse_log_sinh_transform(
     b = b,
     a3_off_transformed_streamflow,
     offset = 0
   )
-  
+
   # add it back to streamflow_data_a3_off
   filtered_streamflow_data_a3_off <- filtered_streamflow_data_a3_off |>
     add_column(
@@ -245,12 +229,10 @@ convert_a3_off_transformed_to_realspace <- function(gauge, streamflow_data_a3_of
     mutate(
       a3_off_modelled_realspace_streamflow = if_else(a3_off_modelled_realspace_streamflow < 0, 0, a3_off_modelled_realspace_streamflow)
     )
-  
-  
+
+
   return(filtered_streamflow_data_a3_off)
 }
-
-
 
 
 streamflow_data_with_a3_off <- map(
@@ -260,8 +242,6 @@ streamflow_data_with_a3_off <- map(
   best_model_per_gauge = only_CO2_model_parameters
 ) |>
   list_rbind()
-
-
 
 
 # Compare percentage difference in a3 on vs. a3 off and save -------------------
@@ -285,13 +265,10 @@ a3_on_off_difference_data <- streamflow_data_with_a3_off |>
   )
 
 
-
-
-
 # Add best non-CO2 streamflow to modelled_streamflow_with_CO2_on_off -----------
 only_non_CO2_models <- best_CO2_non_CO2_per_gauge |>
-  filter(!str_detect(streamflow_model, "CO2")) |> 
-  select(gauge, streamflow_model) |> 
+  filter(!str_detect(streamflow_model, "CO2")) |>
+  select(gauge, streamflow_model) |>
   distinct()
 
 
@@ -299,7 +276,7 @@ non_CO2_model_streamflow <- streamflow_results |>
   semi_join(
     only_non_CO2_models,
     by = join_by(gauge, streamflow_model)
-  ) |> 
+  ) |>
   select(
     gauge,
     year,
@@ -309,14 +286,12 @@ non_CO2_model_streamflow <- streamflow_results |>
   )
 
 
-
-
 ### USE THIS FOR ALL CALCULATIONS ##############################################
-master_streamflow_table <- a3_on_off_difference_data |> 
+master_streamflow_table <- a3_on_off_difference_data |>
   left_join(
     non_CO2_model_streamflow,
     by = join_by(gauge, year, precipitation)
-  ) |> 
+  ) |>
   # rename and rearrange columns
   rename(
     realspace_streamflow_CO2_model_on = realspace_a3_on_streamflow,
@@ -325,16 +300,14 @@ master_streamflow_table <- a3_on_off_difference_data |>
     transformed_streamflow_CO2_model_on = transformed_a3_on_streamflow,
     transformed_streamflow_CO2_model_off = transformed_a3_off_streamflow,
     transformed_streamflow_no_CO2_model = transformed_modelled_streamflow,
-  ) |> 
+  ) |>
   relocate(
-    gauge, year, precipitation, realspace_observed_streamflow, 
-    realspace_streamflow_no_CO2_model, realspace_streamflow_CO2_model_on, 
+    gauge, year, precipitation, realspace_observed_streamflow,
+    realspace_streamflow_no_CO2_model, realspace_streamflow_CO2_model_on,
     realspace_streamflow_CO2_model_off, transformed_observed_streamflow,
     transformed_streamflow_no_CO2_model, transformed_streamflow_CO2_model_on,
     transformed_streamflow_CO2_model_off
   )
-
-
 
 
 write_csv(
@@ -344,88 +317,118 @@ write_csv(
 
 
 # Calculate NSE for all models -------------------------------------------------
-best_model_per_gauge <- best_CO2_non_CO2_per_gauge |> 
-  slice_min(AIC, by = gauge) |> 
-  select(gauge, streamflow_model) |> 
-  distinct() |> 
+best_model_per_gauge <- best_CO2_non_CO2_per_gauge |>
+  slice_min(AIC, by = gauge) |>
+  select(gauge, streamflow_model) |>
+  distinct() |>
   add_column(best_model = TRUE)
 
-all_NSE_results <- streamflow_results |> 
+all_NSE_results <- streamflow_results |>
   summarise(
     NSE_value = nash_sutcliffe_efficiency(
       observed = realspace_observed_streamflow,
       modelled = realspace_modelled_streamflow
     ),
     .by = c(gauge, streamflow_model)
-  ) |> 
-  arrange(gauge, desc(NSE_value)) |> 
+  ) |>
+  arrange(gauge, desc(NSE_value)) |>
   left_join(
     best_model_per_gauge,
     by = join_by(gauge, streamflow_model)
-  ) |> 
+  ) |>
   mutate(
     best_model = if_else(is.na(best_model), FALSE, best_model)
-  ) |> 
+  ) |>
   arrange(best_model)
 
-x <- all_NSE_results |> 
-  ggplot(aes(x = gauge, y = NSE_value, fill = best_model)) + 
-  geom_point(
-    shape = 21,
-    colour = "black"
-    ) + 
-  theme_bw() +
-  theme(
-    axis.text = element_text(angle = 90)
-  )
 
-all_NSE_results |> 
+all_NSE_results |>
   ggplot(aes(x = NSE_value)) +
   geom_histogram(colour = "black", fill = "grey") +
   labs(
     x = "NSE",
     y = "Frequency"
   ) +
-  theme_bw() 
-
-
-# best of the best
-all_NSE_results |> 
-  semi_join(
-    best_model_per_gauge,
-    by = join_by(gauge, streamflow_model)
-  ) |> 
-  pull(NSE_value) |> 
-  summary()
+  theme_bw()
 
 
 # plot NSE vs. number of parameters in a model
 # convert char into function match.fun --> then length(streamflow_model$parameters)
-# match.fun(NSE_parameter_number[1])
+# match.fun(NSE_parameter_number[1]) doesn't work with vector
 # this is a map angle
-NSE_parameter_number <- all_NSE_results |> 
-  pull(streamflow_model) 
+get_parameter_number <- function(streamflow_model_name) {
+  streamflow_function <- match.fun(streamflow_model_name)
+  return(length(streamflow_function()$parameter))
+}
 
+NSE_parameter_number <- all_NSE_results |>
+  # cannot use match.fun with vector. Use rowwise for row-by-row operation
+  rowwise() |>
+  mutate(
+    parameter_number = get_parameter_number(streamflow_model_name = streamflow_model)
+  ) |>
+  ungroup()
+
+
+NSE_parameter_number |>
+  ggplot(aes(x = NSE_value)) +
+  geom_histogram(colour = "black", fill = "grey") +
+  labs(
+    x = "NSE",
+    y = "Frequency"
+  ) +
+  theme_bw() +
+  facet_wrap(~ as.factor(parameter_number))
+
+
+NSE_ecdf <- NSE_parameter_number |>
+  ggplot(
+    aes(
+      x = NSE_value,
+      colour = as.factor(parameter_number)
+    )
+  ) +
+  stat_ecdf(geom = "step", pad = TRUE) +
+  labs(
+    x = "Nash Sutcliffe Efficiency",
+    y = "Cumulative Probability",
+    colour = "Number of Parameters"
+  ) +
+  scale_colour_brewer(palette = "Dark2") +
+  theme_bw() +
+  theme(
+    legend.position = "inside",
+    legend.position.inside = c(0.2, 0.85),
+    legend.background = element_rect(colour = "black", linewidth = 0.2),
+    legend.title = element_text(hjust = 0.5),
+    legend.key.spacing.x = unit(0.5, units = "cm"),
+  ) +
+  guides(
+    colour = guide_legend(
+      override.aes = aes(linewidth = 1.5),
+      ncol = 2
+    )
+  )
 
 ggsave(
-  filename = "Figures/Other/NSE_comparision.pdf",
+  filename = "Figures/Supplementary/NSE_ecdf.pdf",
   device = "pdf",
-  plot = x,
-  width = 1189,
-  height = 500,
+  plot = NSE_ecdf,
+  height = 130,
+  width = 140,
   units = "mm"
 )
 
 
 # Compare NSE values between CO2 and non-CO2 models ----------------------------
 # Does the best model using AIC match NSE?
-best_model_per_gauge <- best_CO2_non_CO2_per_gauge |> 
-  slice_min(AIC, by = gauge) |> 
-  select(gauge, streamflow_model) |> 
-  distinct() |> 
+best_model_per_gauge <- best_CO2_non_CO2_per_gauge |>
+  slice_min(AIC, by = gauge) |>
+  select(gauge, streamflow_model) |>
+  distinct() |>
   mutate(
     AIC_CO2_is_best = str_detect(streamflow_model, "CO2")
-  ) |> 
+  ) |>
   select(gauge, AIC_CO2_is_best)
 
 NSE_value_comparison <- master_streamflow_table |>
@@ -439,23 +442,20 @@ NSE_value_comparison <- master_streamflow_table |>
       modelled = realspace_streamflow_no_CO2_model
     ),
     .by = gauge
-  ) |> 
+  ) |>
   left_join(
     best_model_per_gauge,
     by = join_by(gauge)
-  ) |> 
+  ) |>
   mutate(
     NSE_CO2_is_best = NSE_CO2_model > NSE_non_CO2_model
   )
 
 # For catchments where AIC says CO2 model is best NSE is always higher
 # For catchment where AIC says non-CO2 model is best NSE isn't always higher.
-# They are after very similar. Again, the streamflow values 
+# They are after very similar. Again, the streamflow values
 # are very simlar due to a5 turning off at the last couple year of the timeseries.
 # Additionally, the extra parameters maybe assisting fit.
-
-
-
 
 
 # Timeseries of master table ---------------------------------------------------
@@ -466,12 +466,12 @@ timeseries_plotting_data <- master_streamflow_table |>
     gauge, year, realspace_observed_streamflow,
     realspace_streamflow_no_CO2_model, realspace_streamflow_CO2_model_on,
     realspace_streamflow_CO2_model_off
-  ) |> 
+  ) |>
   pivot_longer(
     cols = starts_with("realspace"),
     names_to = "type",
     values_to = "streamflow"
-  ) |> 
+  ) |>
   mutate(
     type = case_when(
       type == "realspace_streamflow_CO2_model_on" ~ "CO2 Model",
@@ -511,7 +511,6 @@ ggsave(
 )
 
 
-
 # For gauges where CO2 is not the best model there is high overlap with non-CO2
 # model. This is because the a5 term (time of activation term) only kicks in
 # in the last few years of the time series.
@@ -523,12 +522,12 @@ rainfall_runoff_plotting_data <- master_streamflow_table |>
     gauge, precipitation, transformed_observed_streamflow,
     transformed_streamflow_no_CO2_model, transformed_streamflow_CO2_model_on,
     transformed_streamflow_CO2_model_off
-  ) |> 
+  ) |>
   pivot_longer(
     cols = starts_with("transformed"),
     names_to = "type",
     values_to = "streamflow"
-  ) |> 
+  ) |>
   mutate(
     type = case_when(
       type == "transformed_streamflow_CO2_model_on" ~ "CO2 Model",
@@ -542,7 +541,6 @@ rainfall_runoff_plotting_data <- master_streamflow_table |>
   mutate(
     type = factor(type, levels = c("Observed", "CO2 Model", "Counterfactual", "non-CO2 Model"))
   )
-
 
 
 ## massive plot ================================================================
@@ -568,7 +566,7 @@ mega_rainfall_runoff_plot <- rainfall_runoff_plotting_data |>
 
 # why is there a larger difference in the transformed streamflow than
 # realspace streamflow?
-# It is doing weird things at low flows. It attempts to get to as close to 
+# It is doing weird things at low flows. It attempts to get to as close to
 # zero as possible. When in realspace they are all small.
 
 ggsave(
