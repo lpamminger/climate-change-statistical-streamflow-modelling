fe3544d96dfde20e2fc20c6b021284b24ba3f6c2
Lucas Pamminger <lucas.pamminger@monash.edu>
2026-07-07 14:47:33 +1000

    Repeat observed rainfall-runoff relationship for all 81 catchments. Look at aggregated runoff ratio to see if there less streamflow per rainfall.

--- Figures/illustration.R
+++ Figures/illustration.R
@@ -90,9 +90,9 @@ high_record_gauges <- gauge_information |>
 
 ## Try using the simplest CO2 models first
 best_CO2_model_gauge <- best_CO2_and_non_CO2_model_and_params_per_gauge |>
-  filter(contains_CO2) |>
+  filter(contains_CO2) |> 
   filter(gauge %in% high_evi_gauges) |>
-  filter(gauge %in% high_record_gauges) |>
+  #filter(gauge %in% high_record_gauges) |>
   select(gauge, streamflow_model) |>
   distinct() |>
   arrange(desc(streamflow_model))
@@ -119,7 +119,7 @@ replace_CO2_in_data <- function(new_CO2, data) {
 }
 
 
-illustration_plots <- function(gauge, plot_label) {
+illustration_plots <- function(gauge, plot_label, plot_arrow) {
   b <- best_CO2_and_non_CO2_model_and_params_per_gauge |>
     filter(contains_CO2) |>
     filter(gauge == {{ gauge }}) |>
@@ -133,6 +133,12 @@ illustration_plots <- function(gauge, plot_label) {
       q_log_sinh = log_sinh_transform(b = b, y = q_mm, offset = 0)
     )
 
+  
+  # Year range
+  year_range <- modified_data |> 
+    drop_na() |> 
+    pull(year) |> 
+    range()
 
   # Plotting lines for the illustration
   CO2_for_selected_years <- modified_data |>
@@ -154,13 +160,24 @@ illustration_plots <- function(gauge, plot_label) {
     start_stop_indexes = start_stop_indexes
   )
 
+  # if it is a drought model turn off the drought intercept by setting it
+  # to the non-drought intercept
+  non_drought_intercept <- best_CO2_and_non_CO2_model_and_params_per_gauge |>
+    filter(contains_CO2) |>
+    filter(gauge == {{ gauge }}) |>
+    filter(parameter == "a0_n") |> 
+    pull(parameter_value)
 
+  # empty vectors do not play nice
+  if(is_empty(non_drought_intercept)){non_drought_intercept <- NA}
+  
   parameter_set <- best_CO2_and_non_CO2_model_and_params_per_gauge |>
     filter(contains_CO2) |>
     filter(gauge == {{ gauge }}) |>
     # make sure we get a straight line by turning off a2, a4 etc.
     mutate(
       parameter_value = case_when(
+        parameter == "a0_d" ~ non_drought_intercept,
         parameter == "a2" ~ 0,
         parameter == "a4" ~ 0,
         .default = parameter_value
@@ -168,7 +185,9 @@ illustration_plots <- function(gauge, plot_label) {
     ) |>
     pull(parameter_value)
 
-
+  
+  
+  
   streamflow_model <- best_CO2_model_gauge |>
     filter(gauge == {{ gauge }}) |>
     pull(streamflow_model) |>
@@ -197,6 +216,14 @@ illustration_plots <- function(gauge, plot_label) {
   streamflow_4 <- different_CO2_streamflow[[4]] |>
     select(year, precipitation, streamflow_results) |>
     rename(streamflow_2020 = streamflow_results)
+  
+  # if there is no data beyond 2019 for a gauge do not plot it
+  if(max(year_range) < 2019) {
+    streamflow_4 <- streamflow_4 |> 
+      mutate(
+        streamflow_2020 = NA
+      )
+  }
 
   rainfall_runoff_relationship <- streamflow_1 |>
     left_join(
@@ -227,20 +254,22 @@ illustration_plots <- function(gauge, plot_label) {
     )
 
 
-  rainfall_runoff_relationship |>
-    ggplot(aes(x = precipitation, y = streamflow, colour = rainfall_runoff_year)) +
-    geom_line()
 
+  # Arrow aes is hard coded based on the main figure plot
+  # I could rescale it using max and min log-sinh flow and precip?
   if (plot_label == "B") {
-    y_axis_exclude <- element_blank()
+    #y_axis_exclude <- element_blank()
     arrow_aes <- aes(x = 980, y = 180, xend = 980, yend = 60)
   } else {
-    y_axis_exclude <- element_text()
+    #y_axis_exclude <- element_text()
     arrow_aes <- aes(x = 1340, y = 350, xend = 1340, yend = 120)
   }
-
+  
+  
+  
   # put it together now
   plot <- modified_data |>
+    drop_na() |> 
     ggplot(aes(x = p_mm, y = q_log_sinh, fill = year)) +
     geom_line(
       data = rainfall_runoff_relationship,
@@ -252,10 +281,6 @@ illustration_plots <- function(gauge, plot_label) {
       shape = 21,
       size = 3
     ) +
-    geom_segment(
-      mapping = arrow_aes,
-      arrow = arrow(type = "closed", length = unit(2, "mm"))
-    ) +
     labs(
       x = "Annual Precipitation (mm)",
       y = "Log-sinh Annual Streamflow",
@@ -266,6 +291,7 @@ illustration_plots <- function(gauge, plot_label) {
     scale_colour_manual(values = c("#440154FF", "#33638DFF", "#55C667FF", "#B8DE29FF")) +
     # labels hard coded found using data |> filter(year %in% c(1960, 1980, 2000, 2020)) |> pull(CO2) |> unique()
     scale_fill_continuous(
+      limits = c(1959, 2022), # hard code limits based on years (min and max of entire data)
       palette = "viridis", # options: viridis, plasma, RdYlBu
       breaks = c(1960, 1980, 2000, 2020), 
       labels = paste0(c(317, 339, 370, 414), "\n", c("(1960)", "(1980)", "(2000)", "(2020)"))
@@ -275,7 +301,7 @@ illustration_plots <- function(gauge, plot_label) {
       legend.title.position = "top",
       legend.title = element_text(hjust = 0.5),
       plot.title = element_text(vjust = -8, hjust = 0.05, face = "bold"),
-      axis.title.y = y_axis_exclude,
+      #axis.title.y = y_axis_exclude,
       text = element_text(size = 9)
     ) +
     guides(
@@ -289,12 +315,25 @@ illustration_plots <- function(gauge, plot_label) {
         )
     )
 
+  
+  # add arrow is true
+  if(plot_arrow) {
+    plot <- plot + 
+      geom_segment(
+        mapping = arrow_aes,
+        arrow = arrow(type = "closed", length = unit(2, "mm"))
+      ) 
+  }
+  
   return(plot)
 }
 
 
 # plots - pick two gauges to illustration (intercept, slope)
-illustration <- illustration_plots(gauge = "606195", plot_label = "A") | illustration_plots(gauge = "238235", plot_label = "B")
+part_a <- illustration_plots(gauge = "606195", plot_label = "A", plot_arrow = TRUE) 
+part_b <- illustration_plots(gauge = "238235", plot_label = "B", plot_arrow = TRUE) + 
+  theme(axis.title.y = element_blank())
+illustration <- part_a | part_b 
 final_illustration <- illustration + plot_layout(guides = "collect") & theme(legend.position = "bottom")
 
 ggsave(
@@ -306,6 +345,7 @@ ggsave(
   height = 130,
   units = "mm"
 )
+
 
 # add % change for the paragraph -----------------------------------------------
 ## mean annual rainfall ========================================================
@@ -320,6 +360,11 @@ mean_annual_rainfall <- data |>
 
 ## use mean annual rainfall to find streamflow % change ========================
 ### modify illustration plots function to find % change
+### What this code does:
+### - makes log-sinh runoff vs. rainfall lines using the simplest CO2 model (just slope or intercept)
+### - get the log-sinh runoff for the average rainfall
+### - convert the log-sinh runoff back into realspace
+
 percentage_change_based_on_rainfall <- function(gauge, mean_annual_rainfall_data) {
   b <- best_CO2_and_non_CO2_model_and_params_per_gauge |>
     filter(contains_CO2) |>
@@ -409,7 +454,235 @@ model_fit_gauge_B <- percentage_change_based_on_rainfall(gauge = "238235", mean_
 
 
 
+# Repeat plot for all 81 high evidence ratio catchments ------------------------
+## Remove gauges already in chapter 4 ==========================================
+supp_illustration_gauges <- high_evi_gauges[!high_evi_gauges %in% c("606195", "238235")] 
+
+## I want 8 per page - split supp_illustration_gauges ==========================
+chunk <- 8
+n <- length(supp_illustration_gauges)
+split_group <- rep(rep(1:ceiling(n / chunk), each = chunk))[1:n]
+split_supp_illustration_gauges <- split(supp_illustration_gauges, split_group)
+
+## Plot groups =================================================================
+### this is a wrapper for illustration_plots
+grouped_illustration_plot <- function(split_gauges) {
+  map2(
+    .x = split_gauges,
+    .y = letters[1:length(split_gauges)],
+    .f = illustration_plots,
+    plot_arrow = FALSE
+  ) |> 
+    reduce(
+      .f = `+`
+    ) + 
+    plot_layout(
+      nrow = 4, 
+      guides = "collect",
+      axis_titles = "collect"
+    ) & 
+    theme(
+      legend.position = "bottom",
+      plot.margin = margin(t = 0, l = 10, r = 10, b = 0, unit = "pt")
+    )
+}
+
+### Make caption for plots
+#### use the same style as the extended figures
+#### something for gauges 123456 (a), 123456 (b) etc. Same as fig. X.
+create_caption <- function(gauge_chunk, identifier) {
+  abc <- letters[1:length(gauge_chunk)]
+  gauge_abc <- paste0(gauge_chunk, " (", abc, ")")
+  # concatenate everything but last value
+  start_gauge_abc <- paste0(gauge_abc[1:(length(gauge_abc) - 2)], ", ", collapse = "")
+  end_gauge_abc <- paste0(gauge_abc[(length(gauge_abc) - 1)], " and ", gauge_abc[length(gauge_abc)], ".")
+  gauge_text <- paste(c(start_gauge_abc, end_gauge_abc), collapse = "")
+  
+  cat("\\begin{figure}")
+  cat("\n")
+  cat("\t\\centering")
+  cat("\n")
+  cat(paste0("\t\\includegraphics[width=\\textwidth]{Figures/supp_illustration_", identifier, ".pdf}"))
+  cat("\t\n")
+  # The line below must change
+  cat(paste0("\t\\caption{\\textbf{Observed shifts in the rainfall-runoff relationship for gauges ", gauge_text, "} Same as fig X.}"))
+  cat("\n")
+  # The line below must change
+  cat(paste0("\t\\label{fig:supp_illustration_", identifier, "}"))
+  cat("\n")
+  cat("\\end{figure}")
+  cat("\n")
+  cat("\n")
+}
+
+sink("Figures/Other/supp_illustration_gauge_caption.txt")
+iwalk(
+  .x = split_supp_illustration_gauges,
+  .f = create_caption
+)
+sink()
+
+
+# Issues with plot:
+# - not all gauges have data at 1959 thus missing - leave
+# - arrow is in a bad position (likely add argument to turn this on/off) or fix it
+
+#illustration_plots(gauge = supp_illustration_gauges[8], plot_label = "h", plot_arrow = F)
+supp_illustration_plots <- map(
+  .x = split_supp_illustration_gauges,
+  .f = grouped_illustration_plot
+)
+
+
+supp_illustration_plot_names <- paste0("illustration_plot_", 1:length(supp_illustration_plots))
+
+walk2(
+  .x = supp_illustration_plot_names,
+  .y = supp_illustration_plots,
+  .f = ggsave,
+  path = "Figures/Other/",
+  device = "pdf",
+  width = 180,
+  height = 254,
+  units = "mm"
+)
+
+
+# Using observed data does a catchment with the same amount of rainfall receive less runoff? -----
+
+## Aggregate high evidence ratio gauges by decade ==============================
+aggregated_runoff_ratio <- data |> 
+  filter(gauge %in% high_evi_gauges) |> 
+  mutate(
+    decade = year - (year %% 10)
+  ) |> 
+  summarise(
+    # mean account for missing year sum(q) / number of observations not NA
+    decade_mean_q = mean(q_mm, na.rm = TRUE),
+    #use all available rainfall data
+    decade_mean_p = mean(p_mm, na.rm = TRUE),
+    .by = decade
+  ) |> 
+  mutate(
+    runoff_ratio = decade_mean_q / decade_mean_p
+  )
+
+plot_aggregated_runoff_ratio <- aggregated_runoff_ratio |> 
+  drop_na() |> 
+  filter(decade != 1950) |> 
+  ggplot(aes(x = decade, y = runoff_ratio,)) +
+  geom_line() +
+  geom_point() +
+  labs(
+    x = "Decade",
+    y = "Runoff Ratio"
+  ) +
+  theme_bw()
+
+ggsave()
+
+## Aggreate by gauge ===========================================================
+by_gauge_runoff_ratio <- data |> 
+  filter(gauge %in% high_evi_gauges) |> 
+  mutate(
+    decade = year - (year %% 10)
+  ) |> 
+  summarise(
+    # mean account for missing year sum(q) / number of observations not NA
+    decade_mean_q = mean(q_mm, na.rm = TRUE),
+    #use all available rainfall data
+    decade_mean_p = mean(p_mm, na.rm = TRUE),
+    .by = c(decade, gauge)
+  ) |> 
+  mutate(
+    runoff_ratio = decade_mean_q / decade_mean_p
+  )
+
+all_gauge_runoff_ratio <- by_gauge_runoff_ratio |> 
+  drop_na() |> 
+  ggplot(aes(x = decade, y = runoff_ratio)) +
+  geom_line() +
+  geom_point() +
+  labs(
+    x = "Decade",
+    y = "Runoff Ratio"
+  ) +
+  theme_bw() +
+  facet_wrap(~gauge, scale = "free_y")
+
+ggsave(
+  filename = "Figures/Other/mega_runoff_ratio_plot.pdf",
+  plot = all_gauge_runoff_ratio,
+  device = "pdf",
+  width = 1189, 
+  height = 841,
+  units = "mm"
+)
+
+## Aggregate by state ==========================================================
+by_state_runoff_ratio <- data |> 
+  left_join(
+    gauge_information,
+    by = join_by(gauge)
+    ) |> 
+  filter(gauge %in% high_evi_gauges) |> 
+  mutate(
+    decade = year - (year %% 10)
+  ) |> 
+  summarise(
+    # mean account for missing year sum(q) / number of observations not NA
+    decade_mean_q = mean(q_mm, na.rm = TRUE),
+    #use all available rainfall data
+    decade_mean_p = mean(p_mm, na.rm = TRUE),
+    .by = c(decade, state)
+  ) |> 
+  mutate(
+    runoff_ratio = decade_mean_q / decade_mean_p
+  )
+  
+
+# number of gauges by state
+gauge_information |> 
+  filter(gauge %in% high_evi_gauges) |> 
+  count(state)
+
+by_state_runoff_ratio |> 
+  drop_na() |> 
+  filter(decade != 1950) |> 
+  filter(state %in% c("NSW", "TAS", "VIC", "WA")) |> # only 1 ACT, 1 QLD and 1 SA gauge
+  ggplot(aes(x = decade, y = runoff_ratio, colour = state)) +
+  geom_line() +
+  geom_point() +
+  labs(
+    x = "Decade",
+    y = "Runoff Ratio"
+  ) +
+  theme_bw()
 
 
 
 
+
+
+
+
+## Show a change in runoff ratio for Figure 4 in paper =========================
+fig_4_specific_aggregated_runoff_ratio <- data |> 
+  filter(gauge %in% high_evi_gauges) |> 
+  mutate(
+    decade = case_when(
+      year %in% 1990:1999 ~ "1990-1999",
+      year %in% 2012:2021 ~ "2012-2021",
+      .default = NA
+    )
+  ) |>  
+  summarise(
+    # mean account for missing year sum(q) / number of observations not NA
+    decade_mean_q = mean(q_mm, na.rm = TRUE),
+    #use all available rainfall data
+    decade_mean_p = mean(p_mm, na.rm = TRUE),
+    .by = decade
+  ) |> 
+  mutate(
+    runoff_ratio = decade_mean_q / decade_mean_p
+  )
